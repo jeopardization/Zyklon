ea0fe32287c8bea1aebca1529729b5275abeedc3
Tomeno <tomeno@lulzsec.co.uk>
2020-02-23 02:47:50 +0100

    new nametag hp color

--- client/src/respectful/rapist/client/mapping/mappings/FontRenderer.java
+++ client/src/respectful/rapist/client/mapping/mappings/FontRenderer.java
@@ -16,6 +16,15 @@ public class FontRenderer extends MappedClass {
         return 0;
     }
 
+    public int drawStringWithShadow(Object obj, String text, int x, int y, int color) {
+        try {
+            return (int) clazz.getDeclaredMethod("func_78261_a", String.class, int.class, int.class, int.class).invoke(obj, text, x, y, color);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return 0;
+    }
+
     public int getStringWidth(Object obj, String text) {
         try {
             return (int) clazz.getDeclaredMethod("func_78256_a", String.class).invoke(obj, text);
--- client/src/respectful/rapist/client/module/modules/visuals/NameTags.java
+++ client/src/respectful/rapist/client/module/modules/visuals/NameTags.java
@@ -5,6 +5,8 @@ import respectful.rapist.client.EventManager;
 import respectful.rapist.client.mapping.Mappings;
 import respectful.rapist.client.module.Module;
 
+import java.awt.*;
+
 public class NameTags extends Module implements Mappings {
     public NameTags() {
         super(49, "NameTags", "6699FF");
@@ -22,8 +24,6 @@ public class NameTags extends Module implements Mappings {
                 GL11.glRotatef(RenderManager.getPlayerViewX(RenderManager.getInstance()), 1.0F, 0.0F, 0.0F);
                 GL11.glScalef(-scale, -scale, scale);
                 GL11.glDisable(GL11.GL_LIGHTING);
-                GL11.glDepthMask(false);
-                GL11.glDisable(GL11.GL_DEPTH_TEST);
                 GL11.glEnable(GL11.GL_BLEND);
                 OpenGLHelper.glBlendFunc(770, 771, 1, 0);
                 GL11.glDisable(GL11.GL_TEXTURE_2D);
@@ -37,30 +37,22 @@ public class NameTags extends Module implements Mappings {
                 String health = " " + Math.round((EntityLivingBase.getHealth(entityPlayer) / 2.0F) * 10.0F) / 10.0F;
                 String display = name + health;
                 int x = FontRenderer.getStringWidth(Minecraft.getFontRenderer(), display) / 2;
+                GL11.glDepthMask(false);
+                GL11.glDisable(GL11.GL_DEPTH_TEST);
                 Tessellator.addVertex(Tessellator.getInstance(), -x - 1, -1.0D, 0.0D);
                 Tessellator.addVertex(Tessellator.getInstance(), -x - 1, 8.0D, 0.0D);
                 Tessellator.addVertex(Tessellator.getInstance(), x + 1, 8.0D, 0.0D);
                 Tessellator.addVertex(Tessellator.getInstance(), x + 1, -1.0D, 0.0D);
                 Tessellator.draw(Tessellator.getInstance());
                 GL11.glEnable(GL11.GL_TEXTURE_2D);
-                String nameColor = "FFFFFF";
+                int nameColor = 0xFFFFFF;
                 if (EventManager.playerManager.isFriend(name)) {
-                    nameColor = "00C834";
+                    nameColor = 0x00C834;
                 } else if (EventManager.playerManager.isEnemy(name) || (EventManager.moduleManager.aimbot.target != null && EntityPlayer.getCommandSenderName(EventManager.moduleManager.aimbot.target).equalsIgnoreCase(name)) || (EventManager.moduleManager.wTap.target != null && EntityPlayer.getCommandSenderName(EventManager.moduleManager.wTap.target).equalsIgnoreCase(name))) {
-                    nameColor = "FF2A36";
+                    nameColor = 0xFF2A36;
                 }
-                String healthColor = "008E34";
-                if (EntityLivingBase.getHealth(entityPlayer) <= 3.33F) {
-                    healthColor = "D91722";
-                } else if (EntityLivingBase.getHealth(entityPlayer) <= 6.67F) {
-                    healthColor = "F27111";
-                } else if (EntityLivingBase.getHealth(entityPlayer) <= 10.0F) {
-                    healthColor = "FFD700";
-                } else if (EntityLivingBase.getHealth(entityPlayer) <= 13.33F) {
-                    healthColor = "D6E101";
-                } else if (EntityLivingBase.getHealth(entityPlayer) <= 16.67F) {
-                    healthColor = "6BBE48";
-                }
+                float hp = EntityLivingBase.getHealth(entityPlayer);
+                int healthColor = Color.getHSBColor(Math.min(hp / 10F, 2F) * (1F/6F), 1F, 1F).getRGB();
                 FontRenderer.drawStringWithShadow(Minecraft.getFontRenderer(), name, -x, 0, nameColor);
                 FontRenderer.drawStringWithShadow(Minecraft.getFontRenderer(), health, FontRenderer.getStringWidth(Minecraft.getFontRenderer(), name) - x, 0, healthColor);
                 GL11.glEnable(GL11.GL_DEPTH_TEST);
